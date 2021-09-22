9dac0da8575aa93526345f8b4454aa93018a544b
Pedro Henrique <34753501+pedruhb@users.noreply.github.com>
2021-09-22 18:03:31 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,6 +1,12 @@
 <?php
 header('Content-Type: application/json');
 
+if (!isset($_POST['name']) || !isset($_POST['comment']))
+    die(json_encode(array(
+        'status' => "error",
+        'message' => "Missing name and comment."
+    )));
+
 $name = htmlspecialchars($_POST['name']);
 $comment = htmlspecialchars($_POST['comment']);
 
@@ -8,21 +14,21 @@ $pdo = new PDO('mysql:host=localhost;dbname=api', 'root', '', [
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 ]);
 
-$stmt = $pdo->prepare('INSERT INTO comment(name, comment) VALUES(:na, :co)');
-$stmt->bindValue(':na', $name);
-$stmt->bindValue(':co', $comment);
+$stmt = $pdo->prepare('INSERT INTO comment (name, comment) VALUES(?, ?)');
+$stmt->bindParam(1, $name);
+$stmt->bindParam(2, $comment);
 $stmt->execute();
 
-
-if($stmt->rowCount() >= 1) {
-    
+if ($stmt->rowCount() >= 1) {
     $obj = array(
         'status' => "success",
         'name' => $name,
         'comment' => $comment,
     );
     echo json_encode($obj);
-
 } else {
-    echo json_encode('Falha ao salvar comentário');
+    echo json_encode(array(
+        'status' => "error",
+        'message' => "Falha ao salvar comentário"
+    ));
 }
